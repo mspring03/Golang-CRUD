a9ce5595b8dda82b9da8ff25910f95310259bc84
mspring03 <mspringmspring@naver.com>
2021-04-13 15:23:40 +0900

    ❤️

--- Usecase/UserUsecase.go
+++ Usecase/UserUsecase.go
@@ -1,17 +1,50 @@
 package Usecase
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/mspring03/Golang-CRUD/Models"
+	"net/http"
+	"strconv"
+)
 
 type userUsecase struct {
-	r *gin.Engine
 	ur userRepo
 }
 
 type userRepo interface {
-	CreateUser()
+	CreateUser(id string, pw string, age uint8)
+	FindOneId(id string) *Models.User
 }
 
-func NewUserUsecase(r *gin.Engine, ur userRepo) *userUsecase {
-	return &userUsecase{r, ur}
+func NewUserUsecase(ur userRepo) *userUsecase {
+	return &userUsecase{ur}
+}
+
+type signupRequestBody struct {
+	Id       string `json:"id"`
+	Password string `json:"password"`
+	Age      uint8  `json:"age"`
+}
+
+func (uu *userUsecase) Signup(c *gin.Context) {
+	reqBody := new(signupRequestBody)
+	err := c.Bind(reqBody)
+	fmt.Println(*reqBody)
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	user := uu.ur.FindOneId(reqBody.Id)
+	if Collision, _ := strconv.ParseBool(user.ID); Collision == true {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	uu.ur.CreateUser(user.ID, user.Password, user.Age)
+
+	c.JSON(http.StatusCreated, reqBody)
 }
 
