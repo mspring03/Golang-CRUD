819bf57b48ce7671bfa762b9f568675ec24173e0
mspring03 <mspringmspring@naver.com>
2021-04-13 10:33:46 +0900

    ❤️

---
+++ Models/User.go
@@ -0,0 +1,20 @@
+package Models
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"time"
+)
+
+type User struct {
+	ID        uint `gorm:"primaryKey"`
+	Name      string
+	Age       uint8
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func Migtate(db *gorm.DB) {
+	user := db.AutoMigrate(&User{})
+	fmt.Println(user)
+}
