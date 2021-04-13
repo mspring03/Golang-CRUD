0ce93b40a7815b6c9e0975be0bfbdc1b3024eae3
mspring03 <mspringmspring@naver.com>
2021-04-13 15:23:43 +0900

    ❤️

--- Repository/UserRepo.go
+++ Repository/UserRepo.go
@@ -1,15 +1,28 @@
 package Repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/mspring03/Golang-CRUD/Models"
+	"gorm.io/gorm"
+)
 
 type userRepo struct {
 	db *gorm.DB
+	um *Models.User
 }
 
-func UserRepo (db *gorm.DB) *userRepo {
-	return &userRepo{db}
+func UserRepo (db *gorm.DB, um *Models.User) *userRepo {
+	return &userRepo{db, um}
 }
 
-func (r *userRepo) CreateUser() () {
+func (ur *userRepo) CreateUser(id string, pw string, age uint8) {
+	user := Models.User{ID: id, Password: pw, Age: age}
+	fmt.Println(user)
+	ur.db.Create(&user)
+}
 
+func (ur *userRepo) FindOneId(id string) *Models.User {
+	ur.db.Where("ID = ?", id).Find(ur.um)
+
+	return ur.um
 }
