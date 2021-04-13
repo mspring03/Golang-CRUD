7802c76101d3f81347f35cbb7eb229e103768765
mspring03 <mspringmspring@naver.com>
2021-04-13 11:19:55 +0900

    ❤️

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main // import "github.com/mspring03/Golang-CRUD"
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
 
 	"github.com/mspring03/Golang-CURD/Models"
+	"github.com/mspring03/Golang-CURD/Repository"
+	"github.com/mspring03/Golang-CURD/Usecase"
 )
 
 func main() {
@@ -16,9 +17,19 @@ func main() {
 	dsn := os.Getenv("DatabaseUrl")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	if err != nil {
 
-	Models.Migrate(db)
-	fmt.Println(db, err)
+	}
 
-	r.Run(":8080")
+	Models.UserMigrate(db)
+
+	ur := Repository.UserRepo(db)
+
+	uu := Usecase.NewUserUsecase(r, ur)
+
+
+
+
+
+	_ = r.Run(":8080")
 }
