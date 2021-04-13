6149990f37eb340d9cd08ad11f4a9823a68d979f
mspring03 <mspringmspring@naver.com>
2021-04-13 15:23:47 +0900

    ❤️

--- Delivery/UserDelivery.go
+++ Delivery/UserDelivery.go
@@ -1,1 +1,20 @@
 package Delivery
+
+import "github.com/gin-gonic/gin"
+
+type userDelivery struct {
+	uu userUsecase
+}
+
+type userUsecase interface {
+	Signup(c *gin.Context)
+}
+
+func NewUserDelivery(uu userUsecase) *userDelivery {
+	return &userDelivery{uu}
+}
+
+func (ud *userDelivery) Routing(router *gin.RouterGroup) {
+	router.POST("/signup", ud.uu.Signup)
+}
+
