20540baf85fac6b6c856578e601536756cac2690
mspring03 <mspringmspring@naver.com>
2021-04-13 19:10:24 +0900

    ❤️

---
+++ domain/User.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mspring03/Golang-CRUD/Models"
+)
+
+type SignupRequestBody struct {
+	Id       string `json:"id"`
+	Password string `json:"password"`
+	Age      uint8  `json:"age"`
+}
+
+
+type UserUsecase interface {
+	Signup(c *gin.Context)
+}
+
+type UserRepo interface {
+	CreateUser(id string, pw string, age uint8)
+	FindOneId(id string) *Models.User
+}
