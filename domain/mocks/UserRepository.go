8ed535a6ed6a13193e82f87c78efdeffaceb187f
mspring03 <mspringmspring@naver.com>
2021-04-14 11:51:37 +0900

    ❤️

--- domain/mocks/UserRepository.go
+++ domain/mocks/UserRepository.go
@@ -1,1 +1,26 @@
 package mocks
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type ArticleRepository struct {
+	mock.Mock
+}
+
+func (_m *ArticleRepository) Delete(ctx context.Context, id int64) error {
+	ret := _m.Called(ctx, id)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, int64) error); ok {
+		r0 = rf(ctx, id)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+
