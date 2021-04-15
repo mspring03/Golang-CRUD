f04b0dea9a12660df57bcadc695ec9a2e6e04fb5
mspring03 <mspringmspring@naver.com>
2021-04-15 15:42:58 +0900

    ❤️

--- User/usecase/user_ucase.go
+++ User/usecase/user_ucase.go
@@ -16,19 +16,21 @@ func NewUserUsecase(ur domain.UserRepository) *userUsecase {
 	return &userUsecase{ur}
 }
 
-func (uu *userUsecase) Signup(ctx context.Context, a domain.User) (resp gin.H, err error) {
+func (uu *userUsecase) Signup(ctx context.Context, a *domain.User) (resp gin.H, err error) {
 	resp = gin.H{"state": 0, "code": 0, "message": ""}
 
-	_, err := uu.userRepo.IdConflictCheck(ctx, a.Id)
+	_, err = uu.userRepo.IdConflictCheck(ctx, a.Id)
 	if err != nil {
 		switch err {
-		case domain.ErrNotFound:
+		case domain.ErrConflict:
 			resp["state"] = http.StatusNotFound
+			resp["code"] = 0
+			resp["message"] = "Id Already registered"
+			return
 		}
-
 	}
 
-	uu.userRepo.CreateUser(a.Id, a.Password, a.Age)
+	uu.userRepo.CreateUser(ctx, a)
 	token, err := JWT.CreateToken(a.Id)
 	if err != nil {
 
