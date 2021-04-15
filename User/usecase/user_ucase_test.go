ffbc373b7572a1d26cbd3ef4cb778d3c57280241
mspring03 <mspringmspring@naver.com>
2021-04-15 11:18:38 +0900

    ❤️

--- User/usecase/user_ucase_test.go
+++ User/usecase/user_ucase_test.go
@@ -1,1 +1,36 @@
-package usecase
+package usecase_test
+
+import (
+	//"context"
+	//"errors"
+	"testing"
+	//"time"
+	//
+	//"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/mock"
+	//
+	//ucase "github.com/mspring03/Golang-CRUD/user/usecase"
+	"github.com/mspring03/Golang-CRUD/domain"
+	//"github.com/mspring03/Golang-CRUD/domain/mocks"
+)
+
+func TestSignup(t *testing.T) {
+	//mockUserRepo := new(mocks.UserRepository)
+	mockUser := domain.SignupRequestBody{
+		Id: "test_id",
+		Password: "test_password",
+		Age: 19,
+	}
+
+	mockListUser := make([]domain.SignupRequestBody, 0)
+	mockListUser = append(mockListUser, mockUser)
+
+	t.Run("success", func(t *testing.T) {
+
+		t.Log(mockListUser)
+	})
+
+	t.Run("error-failed", func(t *testing.T) {
+		t.Log(mockListUser)
+	})
+}
