984e2dc90a92c5fe835fb3c4e8759367ceac5417
mspring03 <mspringmspring@naver.com>
2021-04-15 19:18:59 +0900

    ❤️

--- User/delivery/http/middleware/middleware.go
+++ User/delivery/http/middleware/middleware.go
@@ -1,1 +1,16 @@
 package middleware
+
+import "github.com/gin-gonic/gin"
+
+type GoMiddleware struct {
+	// another stuff , may be needed by middleware
+}
+
+// CORS will handle the CORS middleware
+func (m *GoMiddleware) CORS(next gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) error {
+		c.Header("Access-Control-Allow-Origin", "*")
+
+		return next(c)
+	}
+}
