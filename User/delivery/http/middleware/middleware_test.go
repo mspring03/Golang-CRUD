d6177e089cb0123961ad92d0d4851b9289f4a6f0
mspring03 <mspringmspring@naver.com>
2021-04-16 11:46:55 +0900

    ❤️

--- User/delivery/http/middleware/middleware_test.go
+++ User/delivery/http/middleware/middleware_test.go
@@ -1,1 +1,29 @@
-package middleware
+package middleware_test
+
+import (
+	"net/http"
+	test "net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mspring03/Golang-CRUD/user/delivery/http/middleware"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCORS(t *testing.T) {
+	e := gin.New()
+	_ = test.NewRequest(http.MethodGet, "/", nil)
+	res := test.NewRecorder()
+
+	c, _ := gin.CreateTestContext(res)
+	m := middleware.InitMiddleware()
+
+	h := m.CORS(gin.HandlerFunc(func(c *gin.Context) error {
+		return c.ContentType(http.StatusOK)
+	}))
+
+
+	require.NoError(t, err)
+	assert.Equal(t, "*", res.Header().Get("Access-Control-Allow-Origin"))
+}
