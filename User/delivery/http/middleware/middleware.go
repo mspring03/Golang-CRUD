b384e838f007f77d7a3b21cfa61ff7a45d16c64a
mspring03 <mspringmspring@naver.com>
2021-04-16 11:46:57 +0900

    ❤️

--- User/delivery/http/middleware/middleware.go
+++ User/delivery/http/middleware/middleware.go
@@ -8,21 +8,17 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
 
 type GoMiddleware struct {
 
 }
 
-func (m *GoMiddleware) SetHeader(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0")
-	c.Header("Last-Modified", time.Now().String())
-	c.Header("Pragma", "no-cache")
-	c.Header("Expires", "-1")
-
-	c.Next()
+func (m *GoMiddleware) CORS(next gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		next(c)
+	}
 }
 
 func (m *GoMiddleware) VerifyToken(c *gin.Context) {
